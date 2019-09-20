cac07b847f915cecb84339daf37cd09650e8b025
Marek <mareklani@gmail.com>
2019-09-20 19:24:56 +0300

    Backend API implemented

--- AlarmBackendAPI/Controllers/EntriesController.cs
+++ AlarmBackendAPI/Controllers/EntriesController.cs
@@ -12,8 +12,6 @@ namespace AlarmBackendAPI.Controllers
     [ApiController]
     public class EntriesController : Controller
     {
-
-        CosmosClient cosmosClient;
         CosmosHelper cosmosHelper;
 
         public EntriesController(CosmosHelper ch)
@@ -26,6 +24,39 @@ namespace AlarmBackendAPI.Controllers
         public async Task<ActionResult> GetLastEntryForName([FromQuery]string name)
         {
             return Ok(await cosmosHelper.QueryItemsAsync($"SELECT TOP 1 *  FROM c WHERE CONTAINS(c.Entrants, '{name}') ORDER BY c.Timestamp DESC"));
+        }
+
+        [HttpGet]
+        [Route("GetLastEntry")]
+        public async Task<ActionResult> GetLastEntry()
+        {
+            return Ok(await cosmosHelper.QueryItemsAsync($"SELECT TOP 1 *  FROM c ORDER BY c.Timestamp DESC"));
+        }
+
+        [HttpGet]
+        [Route("LastKnownEntrant")]
+        public async Task<ActionResult> LastKnownEntrant()
+        {
+            return Ok(await cosmosHelper.QueryItemsAsync($"SELECT TOP 1 *  FROM c WHERE c.Entrants != '' ORDER BY c.Timestamp DESC"));
+        }
+
+        [HttpGet]
+        [Route("TodaysEntrants")]
+        public async Task<ActionResult> TodaysEntrants()
+        {
+            var res = await cosmosHelper.QueryItemsAsync($"SELECT *  FROM c WHERE c.Entrants != '' AND c.Timestamp > '{DateTime.Now.Date.ToString("s")}'");
+            List<string> entrants = new List<string>();
+            foreach(var e in res)
+            {
+                var names = e.Entrants.Split('|');
+                foreach(var n in names)
+                {
+                    if (n != "" && !entrants.Contains(n))
+                        entrants.Add(n);
+                }
+            }
+
+            return Ok(entrants);
         }
     }
 }
